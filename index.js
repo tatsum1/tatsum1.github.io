5292d0681865f2e8144d682739e1d9d2e9d32aaf
Igor <iskovigor19@gmail.com>
2019-07-18 15:02:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// function openbox(box){
+//       display = document.getElementById(box).style.display;
+//       if(display=='none'){
+//             document.getElementById(box).style.display='block';
+//       }else{
+//             document.getElementById(box).style.display='none';
+//       }
+// }
+
+
+
+function openMenu(){
+	document.getElementById("box").classList.toggle('active');
+}
